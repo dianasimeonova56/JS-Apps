29660f4584a58b8542e8f9e28a05a6e6111acc9e
Diana Simeonova <122699903+dianasimeonova56@users.noreply.github.com>
2023-11-02 20:50:12 +0200

    Add files via upload

---
+++ Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -0,0 +1,59 @@
+/*function loadRepos() {
+	const username = document.getElementById("username").value;
+	const list = document.getElementById('repos');
+
+	fetch(`https://api.github.com/users/${username}/repos`)
+	.then(handleREsponse)
+	.then(displayRepos)
+	.catch(handleError);
+
+	function handleREsponse(response) {
+		if(response.ok == false) {
+			throw new Error(`${response.status} ${response.statusText}`);
+		}
+		return response.json();
+	}
+
+	function displayRepos(data) {
+		list.innerHTML = '';
+		//data => console.log(data.map(({full_name, html_url}) => ({full_name, html_url})))
+		for(let repo of data) {
+			list.innerHTML += `<li>
+			<a href="${repo.html_url}" target= "_blank">
+				${repo.full_name}
+			</a>
+		</li>`
+		}
+	}
+	function handleError(error) {
+		list.innerHTML = `${error.message}`
+	}
+}*/
+
+async function loadRepos() {
+	const username = document.getElementById('username').value;
+	const list = document.getElementById('repos');
+
+	try {
+		const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+		if(response.ok == false) {
+			throw new Error(`${response.status} ${response.statusText}`);
+		}
+
+		const data = await response.json();
+
+		list.innerHTML = '';
+
+		for(let repo of data) {
+			list.innerHTML += `<li>
+			<a href="${repo.html_url}" target= "_blank">
+				${repo.full_name}
+			</a>
+		</li>`
+		}
+		
+	} catch (error) {
+		list.innerHTML = `${error.message}`;
+	}
+}
---
+++ Asynchronous Programming/05. Accordion/accordion.js
@@ -0,0 +1,63 @@
+async function solution() {
+    const main = document.getElementById('main');
+
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    data.forEach(a => {
+        let divAccordion = createElement('div', '', ['class', 'accordion']);
+        let divHead = createElement('div', '', ['class', 'head']);
+        let span = createElement('span', a.title);
+        let button = createElement('button', 'More', ['class', 'button', 'id', a._id])
+        let divExtra = createElement('div', '', ['class', 'extra']);
+        let p = createElement('p')
+
+        button.addEventListener('click', toggle);
+
+        divExtra.appendChild(p)
+        divHead.appendChild(button);
+        divHead.appendChild(span);
+        divAccordion.appendChild(divHead);
+        divAccordion.appendChild(divExtra);
+        main.appendChild(divAccordion);
+
+    });
+
+    async function toggle(event) {
+        const accordion = event.target.parentNode.parentNode;
+        const p = accordion.children[1].children[0];
+
+        const extra = accordion.children[1];
+        console.log(accordion)
+        const id = event.target.id;
+        let url = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        p.textContent = data.content;
+
+        const hidden = event.target.textContent === 'More';
+        extra.style.display = hidden ? 'block' : 'none';
+        event.target.textContent = hidden ? 'Less' : 'More';
+
+    }
+
+    function createElement(type, content, attributes = []) {
+        const element = document.createElement(type);
+
+        if (content) {
+            element.textContent = content;
+        }
+
+        if (attributes.length > 0) {
+            for (let i = 0; i < attributes.length; i += 2) {
+                element.setAttribute(attributes[i], attributes[i + 1]);
+            }
+        }
+        return element;
+    }
+}
+solution();
